c63f61b7f1b2805b67ecf59ca3f767a8cd3f4147
いちご <49925651+nynicg@users.noreply.github.com>
2020-04-02 05:28:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 git pull https://github.com/nynicg/graduation
 
-g++ main.cpp -o diyusi `pkg-config --libs --cflags opencv`
+g++ main.cpp -o diyusi `pkg-config --libs --cflags opencv` -std=c++11
 
 chmod 755 diyusi
